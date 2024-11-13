4cce48d1a0d22b168d5b37f5a016358790885de0
zliipo1 <plastjoe17@gmail.com>
2024-11-13 10:09:22 +0100

    Create program.py

---
+++ program.py
@@ -0,0 +1,17 @@
+inventory = []
+name = input("hej, vad heter du?")
+greeting = "välkomen till [Elamins] värld. du vaknar upp efter en lång natts sömn..." 
+
+print(greeting)
+print("Mystisk nog finner du dig i en hamsters kropp, där du löper över ängarna i jakt på en en gyllene markos")
+print("du rycker till och fryser i din tanke tittar du på [maskroen], eller din mystiska [hamsters]- kropp")
+choice = input("vad, väljer du?").lower()
+if "hamster" in choice:
+    print("I drömlik")
+elif "maskros" in choice:
+    print("du skriver inombords som den maskros")
+    inventory 
+else: 
+    print("ok")
+    print("men se där...") 
+    
